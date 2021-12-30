f619d9eebea44e6f1986ab0144376423a273624e
nicokant <niccolocantu@gmail.com>
2021-12-30 19:19:51 +0100

    layout definition with placeholders

---
+++ src/components/map.js
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { MapContainer, TileLayer, LayersControl } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css';
+import config from '../config';
+
+const { BaseLayer } = LayersControl
+
+export default function Map() {
+    return (
+        <div>
+            <MapContainer center={config.center} zoom={config.zoom} scrollWheelZoom={false}>
+                <LayersControl>
+                {config.layers.map(l => (
+                    <BaseLayer
+                        key={l.name}
+                        name={l.name}
+                        checked={l.checked}
+                    >
+                        <TileLayer
+                            attribution={l.attribution}
+                            url={l.url}
+                        />
+                    </BaseLayer>))
+                }
+                </LayersControl>
+            </MapContainer>
+        </div>
+    )
+}
---
+++ src/config.js
@@ -0,0 +1,14 @@
+export default {
+    osm_description: '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>',
+    osm_italy_description: '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>',
+    center: [51.505, -0.09],
+    zoom: 13,
+    layers: [
+        {
+            checked: true,
+            name: 'OSM',
+            url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }
+    ]
+}
