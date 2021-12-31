73d6b81b50fb9c8892a6a2c0921da119c03be1b8
nicokant <niccolocantu@gmail.com>
2021-12-31 11:55:06 +0100

    add projects, add contacts, move texts to config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 export default {
-    osm_description: '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>',
-    osm_italy_description: '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>',
+    osmTitle: 'OpenStreetMap',
+    osmDescription: '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>', // supports HTML
+    osmItalyTitle: 'OpenStreetMap Italia',
+    osmItalyDescription: '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>', // supports HTML
     center: [51.505, -0.09],
     zoom: 13,
     layers: [
@@ -10,5 +12,48 @@ export default {
             url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }
-    ]
+    ],
+    projects: [
+        {
+            image: 'icon',
+            name: 'Progetto di esempio',
+            description: 'Breve descrizione di questo progetto',
+            link: 'https://google.com',
+        },
+        {
+            image: 'icon',
+            name: 'Progetto di esempio',
+            description: 'Breve descrizione di questo progetto',
+            link: 'https://google.com',
+        },
+        {
+            image: 'icon',
+            name: 'Progetto di esempio',
+            description: 'Breve descrizione di questo progetto',
+            link: 'https://google.com',
+        },
+        {
+            image: 'icon',
+            name: 'Progetto di esempio',
+            description: 'Breve descrizione di questo progetto',
+            link: 'https://google.com',
+        },
+        {
+            image: 'icon',
+            name: 'Progetto di esempio',
+            description: 'Breve descrizione di questo progetto',
+            link: 'https://google.com',
+        },
+    ],
+    projectsDescription: '', // supports HTML
+    contactDescription: '', // supports HTML
+    contactTitle: 'Contatti',
+    github: {
+        url: 'https://google.com',
+        text: 'Visita GitHub'
+    },
+    facebook: {
+        url: 'https://google.com',
+        text: ''
+    }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,90 @@
 import * as React from "react"
 
-import { StaticImage } from 'gatsby-plugin-image'
+import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image'
 import HTMLRenderer from "react-html-renderer"
+
+import { graphql } from 'gatsby';
 
 import Map from '../components/map';
 
 import config from '../config';
+import { Facebook, GitHub } from "react-feather";
 
-const IndexPage = () => {
+
+const IndexPage = ({ data }) => {
+  console.log(data);
+
+  const imageMap = data.allFile.edges.map(e => ({ [e.node.name]: getImage(e.node) }))
+    .reduce((p,c) => Object.assign(p,c), {})
+
+  console.log(imageMap);
   return (
     <>
       <header className="bg-black py-5">
-        <div className="container mx-auto flex">
+        <div className="max-w-6xl p-2 mx-auto md:p-4 flex">
           <div className="flex">
-            <StaticImage className="mr-5" alt="logo Wikimedia" src="../images/WIKIItaly.png" width={50} height={50} layout="fixed" />
-            <StaticImage alt="logo OSM Italia" src="../images/OSMItaly.svg" width={50} height={50} layout="fixed" />
+            <StaticImage className="mr-5" alt="logo Wikimedia" src="../images/WIKIItaly.png" width={50} height={50} layout="fixed" loading="eager" />
+            <StaticImage alt="logo OSM Italia" src="../images/OSMItaly.svg" width={50} height={50} layout="fixed" loading="eager" />
           </div>
           <div className="ml-auto text-white self-center uppercase">
-            <a className="mx-2">Home</a>
+            <a className="mx-2" href="#">Home</a>
             <a className="mx-2" href="#chisiamo">Chi Siamo</a>
             <a className="mx-2" href="#progetti">Progetti</a>
             <a className="mx-2" href="#contatti">Contatti</a>
           </div>
         </div>
       </header>
-      <main className="container mx-auto">
+      <main className="max-w-6xl p-2 mx-auto md:p-4">
         <section className="my-10">
-          <h2 className="text-3xl flex mb-2 font-bold"><StaticImage alt="" className="mr-5" src="../images/osm.svg" width={50} layout="fixed" />OpenStreetMap</h2>
+          <h2 className="text-3xl flex mb-2 font-bold"><StaticImage loading="eager" alt="" className="mr-5" src="../images/osm.svg" width={50} layout="fixed" />{config.osmTitle}</h2>
           <hr />
           <div className="prose lg:prose-lg mt-5 max-w-full mb-5">
-            <HTMLRenderer html={config.osm_description} />
+            <HTMLRenderer html={config.osmDescription} />
           </div>
           <Map />
         </section>
         <section className="my-10" id="chisiamo">
-          <h2 className="text-3xl flex mb-2 font-bold"><StaticImage alt="" className="mr-5" src="../images/osm.svg" width={50} layout="fixed" />OpenStreetMap Italia</h2>
+          <h2 className="text-3xl flex mb-2 font-bold"><StaticImage alt="" className="mr-5" src="../images/osm.svg" width={50} layout="fixed" />{config.osmItalyTitle}</h2>
           <hr />
           <div className="prose lg:prose-lg mt-5 max-w-full">
-            <HTMLRenderer html={config.osm_italy_description} />
+            <HTMLRenderer html={config.osmItalyDescription} />
           </div>
           <StaticImage src="../images/image.png" layout="fullWidth" className="mx-auto mt-5 mx-10" />
         </section>
-        <section id="progetti">
+        <section id="progetti" className="mb-10">
           <h2 className="text-3xl flex mb-2 font-bold"><StaticImage alt="" className="mr-5" src="../images/osm.svg" width={50} layout="fixed" />Progetti</h2>
           <hr />
+          <div className="prose lg:prose-lg mt-5 max-w-full mb-5">
+            <HTMLRenderer html={config.projectsDescription} />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 my-5">
+            {config.projects.map(p => (
+              <div className="flex gap-4" key={p.name}>
+                <GatsbyImage image={imageMap[p.image]} alt="" />
+                <div>
+                  <p className="text-lg font-bold">{p.name}</p>
+                  <p className="mb-5">{p.description}</p>
+                  <a href={p.link} className="mt-1 px-3 py-2 bg-red-500 rounded">Vai</a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+        <section id="contatti">
+        <h2 className="text-3xl font-bold mb-2">{config.contactTitle}</h2>
+          <hr />
+          <div className="prose lg:prose-lg mt-5 max-w-full mb-5">
+            <HTMLRenderer html={config.contactDescription} />
+          </div>
+          <div className="flex gap-4 items-center mt-5">
+            {config.github && <a href={config.github.url} target="blank" className="flex gap-2"><GitHub />{config.github.text}</a>}
+            {config.facebook && <a href={config.facebook.url} target="blank" className="flex gap-2"><Facebook />{config.facebook.text}</a>}
+          </div>
         </section>
       </main>
-      <footer id="contatti">
-        <div className="container">
-
+      <footer>
+        <div className="max-w-6xl p-2 mx-auto md:p-4 my-5">
+          
         </div>
       </footer>
     </>
@@ -56,3 +92,24 @@ const IndexPage = () => {
 }
 
 export default IndexPage
+
+export const indexQuery = graphql`
+  query AssetsPhotos {
+    allFile(filter: {extension: {regex: "/(jpg)|(jpeg)|(png)/"}, dir: {regex: "/images/projects/"}}) {
+      edges {
+        node {
+          id
+          name
+          childImageSharp {
+            gatsbyImageData(
+              width: 150
+              height: 150
+              layout: FIXED
+              placeholder: BLURRED
+            )
+          }
+        }
+      }
+    }
+  }
+`
