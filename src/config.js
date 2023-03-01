bfb72371c6542a31b69deb6b1a28ec8536452bc0
Lorenzo Stucchi <calendariodioreste@gmail.com>
2023-03-01 19:10:03 +0100

    add working link

--- src/config.js
+++ src/config.js
@@ -76,7 +76,7 @@ export default {
             image: 'umap',
             name: 'uMap',
             description: 'Crea una mappa in pochi minuti usando i dati di OpenStreetMap',
-            link: 'https://osmit-umap.wmcloud.org/it/',
+            link: 'https://umap.openstreetmap.fr/it/',
         },
         {
             image: 'suosm',
